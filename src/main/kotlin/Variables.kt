c669b0af8bcdedf9c4b9655c028fc338bb98c3f4
aronmakiika <aronmakiika@gmail.com>
2022-05-05 21:25:13 +0300

    chore: initial commit

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val fullName: String = "Makiika Aron"
+    val nationality = "Ugandan"
+
+    println("$fullName is a $nationality by birth")
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val name: String = "makiika aron"
+    println(name)
+}
