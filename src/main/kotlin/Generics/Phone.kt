58d87ea6046d86944e351ca810c043f5dc11751d
aronmakiika <aronmakiika@gmail.com>
2022-06-08 10:05:42 +0300

    chore: add phone class

--- src/main/kotlin/Generics/Phone.kt
+++ src/main/kotlin/Generics/Phone.kt
@@ -1,8 +1,6 @@
 package Generics
 
-
-//open class Phone( val os:String) {
-//    fun printFonOs() = "Your phon's os is $os"
-//}
-
+open class Phone( val os:String) {
+    fun printFonOs() = "Your phon's os is $os"
+}
 
