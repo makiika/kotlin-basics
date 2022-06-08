aa03ef5733b5649421972368017e72409f3180b8
aronmakiika <aronmakiika@gmail.com>
2022-06-08 10:00:46 +0300

    chore: delete printFonOs function

--- src/main/kotlin/Generics/Phone.kt
+++ src/main/kotlin/Generics/Phone.kt
@@ -1,6 +1,14 @@
 //package Generics
 
+
 open class Phone( val os:String) {
     fun printFonOs() = "Your phon's os is $os"
 }
 
+<<<<<<< HEAD
+=======
+
+
+
+
+>>>>>>> d395909 (chore: delete printFonOs function)
